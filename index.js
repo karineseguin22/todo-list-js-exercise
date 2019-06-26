0592520b7f28b103dd5427d714c35c0b2decc96a
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-26 20:18:19 +0000

    Add description function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescription = []; 
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
@@ -21,11 +22,21 @@ function logTaskState(taskIndex) {
   console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
 
+//function that adds description of new task
+function newTask(title, description){
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", 'Take all the 💩 out of the litter box"'); 
+const task2 = newTask("Do Laundry", "😨"); 
+const tasks = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+//testing
+console.log(tasks);
