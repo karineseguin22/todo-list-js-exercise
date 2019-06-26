0dc1bedd88a387c7e8a189f1de02230a3631b0fd
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-26 20:44:04 +0000

    Fix bug, there was a comma missing begore logState

--- index.js
+++ index.js
@@ -10,22 +10,21 @@ function newTask(title) {
   taskComplete.push(false);
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
 
 //function that adds description of new task
 function newTask(title, description){
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false, 
+  
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
 }
@@ -36,9 +35,6 @@ const task1 = newTask("Clean Cat Litter", 'Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨"); 
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-//testing
-console.log(tasks);
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
