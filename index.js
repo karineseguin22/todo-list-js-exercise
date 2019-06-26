fa0d8074f172954b3484e60bce9179430c759091
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-26 20:25:17 +0000

    Refactor logTaskState and completeTask functions

--- index.js
+++ index.js
@@ -11,15 +11,13 @@ function newTask(title) {
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+function completeTask(task) {
+  task.complete = true;
 }
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 //function that adds description of new task
@@ -37,6 +35,10 @@ function newTask(title, description){
 const task1 = newTask("Clean Cat Litter", 'Take all the 💩 out of the litter box"'); 
 const task2 = newTask("Do Laundry", "😨"); 
 const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
 
 //testing
 console.log(tasks);
